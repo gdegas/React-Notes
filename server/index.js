abbc39484dc8d5924d7b9146d71b1afba8a606dd
gdegas001 <gdegas001@gmail.com>
2017-08-01 15:21:53 -0700

    Added and required body-parser.#3

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const getNotes = require('./notes')
 const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const publicPath = path.join(__dirname, 'public')
 const staticMiddleware = express.static(publicPath)
 
+app.use(bodyParser)
 app.use(staticMiddleware)
 
 app.get('/notes', (req, res) => {
