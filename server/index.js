bbf932c38a890c9197b5a7d27e990a4094d2a75c
gdegas001 <gdegas001@gmail.com>
2017-08-01 15:48:24 -0700

    Defined POST route to add data to table.#3

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const getNotes = require('./notes')
+const crudNotes = require('./crud-notes')
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -6,12 +6,28 @@ const app = express()
 const publicPath = path.join(__dirname, 'public')
 const staticMiddleware = express.static(publicPath)
 
-app.use(bodyParser)
+app.use(bodyParser.json())
 app.use(staticMiddleware)
 
 app.get('/notes', (req, res) => {
-  getNotes()
+  crudNotes.getNotes()
     .then(notes => res.json(notes))
+    .catch(error => {
+      res.sendStatus(500)
+      console.log(error)
+    })
+})
+
+app.post('/notes', (req, res) => {
+  const noteBody = req.body
+  crudNotes.addNote(noteBody)
+    .then(report => {
+      res.sendStatus(201)
+    })
+    .catch(error => {
+      console.log(error)
+      res.sendStatus(500)
+    })
 })
 
 app.listen(3000, () => console.log('listening on 3000'))
