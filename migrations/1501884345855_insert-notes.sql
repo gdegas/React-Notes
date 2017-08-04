aca2b9f53c5a5b1d9feb97d18885e5a4526b8f45
gdegas001 <gdegas001@gmail.com>
2017-08-04 15:25:21 -0700

    Created migrations for database tables

---
+++ migrations/1501884345855_insert-notes.sql
@@ -0,0 +1,11 @@
+-- up
+INSERT INTO notes (title, content)
+VALUES
+  ('Grocery list', 'apples, orange, chicken, bread, bananas, diapers, napkins'),
+  ('To Do', 'Wash the car, clean the room, register at DMV, call back girlfriend'),
+  ('Reminders', 'Learn SQL!, read javascript books!, watch React videos, read more');
+
+---
+
+-- down
+TRUNCATE TABLE notes;
